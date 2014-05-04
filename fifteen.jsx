b22bd6094e0a2b249e0bea81e5ad16a34a8c5e96
Mikhail Menshenin <greggsoft@gmail.com>
2014-05-04 11:47:11 +0600

    add game init

--- fifteen.jsx
+++ fifteen.jsx
@@ -15,7 +15,7 @@ var Board = React.createClass({
     getInitialState : function() {
         return { cells : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0] };
     },
-    move : function(i, j) {
+    flip : function(i, j) {
         console.log("flip " + i + " and " + j);
         var cells = this.state.cells;
         var prev = cells[i];
@@ -23,51 +23,74 @@ var Board = React.createClass({
         cells[j] = prev;
         this.setState({ cells: cells});
     },
-    up : function() {
-        console.log("try to move up");
-        var cells = this.state.cells;
-        var zeroIndex = cells.indexOf(0);
-        if (zeroIndex - 4 >= 0) {
-            this.move(zeroIndex, zeroIndex - 4);
+    directions : {
+        up : {
+            can : function(zeroIndex) {
+                return zeroIndex - 4 >= 0;
+            },
+            newIndex : function(zeroIndex) {
+                return zeroIndex - 4;
+            }
+        },
+        down : {
+            can : function(zeroIndex) {
+                return zeroIndex + 4 < 16;
+            },
+            newIndex : function(zeroIndex) {
+                return zeroIndex + 4;
+            }
+        },
+        left : {
+            can : function(zeroIndex) {
+                return zeroIndex % 4 != 0;
+            },
+            newIndex : function(zeroIndex) {
+                return zeroIndex - 1;
+            }
+        },
+        right : {
+            can : function(zeroIndex) {
+                return (zeroIndex + 1) % 4 != 0;
+            },
+            newIndex : function(zeroIndex) {
+                return zeroIndex + 1;
+            }
         }
     },
-    down : function() {
-        console.log("try to move down");
+    move : function(direction) {
+        console.log("try to move " + direction);
         var cells = this.state.cells;
         var zeroIndex = cells.indexOf(0);
-        if (zeroIndex + 4 < 16) {
-            this.move(zeroIndex, zeroIndex + 4);
+        if (this.directions[direction].can(zeroIndex)) {
+            this.flip(zeroIndex, this.directions[direction].newIndex(zeroIndex));
         }
-    },
-    left : function() {
-        console.log("try to move left");
-        var cells = this.state.cells;
-        var zeroIndex = cells.indexOf(0);
-        if (zeroIndex % 4 != 0) {
-            this.move(zeroIndex, zeroIndex - 1);
-        }
-    },
-    right : function() {
-        console.log("try to move right");
-        var cells = this.state.cells;
-        var zeroIndex = cells.indexOf(0);
-        if ((zeroIndex + 1) % 4 != 0) {
-            this.move(zeroIndex, zeroIndex + 1);
-        }
+        return this.directions[direction].can(zeroIndex);
     },
     keyDown : function(event) {
         console.log("key down with code " + event.keyCode);
         if (event.keyCode == 37) {
-            this.right();
+            this.move("right");
         }
         if (event.keyCode == 38) {
-            this.down();
+            this.move("down");
         }
         if (event.keyCode == 39) {
-            this.left();
+            this.move("left");
         }
         if (event.keyCode == 40) {
-            this.up();
+            this.move("up");
+        }
+    },
+    init: function() {
+        var count = parseInt(this.props.steps);
+        console.log("init game with " + count + " steps");
+        var dirs = [ "up", "down", "left", "right" ];
+        var dirIndex = 0;
+        while (count > 0) {
+            dirIndex = Math.floor(Math.random() * 4);
+            if (this.move(dirs[dirIndex])) {
+                count--;
+            }
         }
     },
     render : function() {
@@ -82,7 +105,9 @@ var Board = React.createClass({
     }
 });
 
-var board = React.renderComponent(<Board />, document.body);
+var board = React.renderComponent(<Board steps="100" />, document.body, function() {
+    this.init();
+});
 
 window.onkeydown = board.keyDown;
 
